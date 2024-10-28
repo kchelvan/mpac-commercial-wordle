60736e26270115a0276e051da225d0214639f465
kchelvan <keshopan.chelvan@gmail.com>
2024-10-27 20:52:38 -0400

    Integrated Validation API to complete logic for Wordle Guesses

--- src/app/features/board/index.tsx
+++ src/app/features/board/index.tsx
@@ -1,28 +1,32 @@
 'use client';
 import React from 'react';
 import styles from './board.module.css';
-import { BOARD_HEIGHT, BOARD_WIDTH } from '@/app/utils/const';
 import { useKeydown } from '@/app/hooks/useKeydown';
+import { BOARD_HEIGHT, BOARD_WIDTH } from '@/app/utils/const';
+import { getCellStyle, getCellValue } from './helpers';
 
-const Board = () => {
-	const { userInput } = useKeydown();
+export const Board = () => {
+	const { guesses } = useKeydown();
 	const boardCells = Array(BOARD_HEIGHT * BOARD_WIDTH).fill(0);
 
 	return (
 		<form className={styles.container}>
 			<div className={styles.board_container}>
-				{boardCells.map((_, idx) => (
-					<div
-						key={idx}
-						className={`${styles.board_cell} ${
-							userInput[idx]
-								? styles.board_active_cell
-								: styles.board_inactive_cell
-						}`}
-					>
-						<p className={styles.board_letter}>{userInput[idx]}</p>
-					</div>
-				))}
+				{boardCells.map((_, idx) => {
+					const cellData = getCellValue(idx, guesses);
+					const column = idx % 5;
+					return (
+						<div
+							key={idx}
+							className={`${styles.board_cell} ${getCellStyle(
+								cellData,
+								column
+							)}`}
+						>
+							<p className={styles.board_letter}>{cellData.value}</p>
+						</div>
+					);
+				})}
 			</div>
 		</form>
 	);
